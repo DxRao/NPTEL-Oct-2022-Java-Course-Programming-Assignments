328e9679d81507eb3240e68b4fa9da05bb19a064
D Rao <tdayakarrao@gmail.com>
2022-12-18 14:06:18 +0530

    Week-1: Programming Assignments

--- W1PA/src/W1PA1.java
+++ W1PA/src/W1PA1.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
 
 public class W1PA1 {
-
+       public static void main(String[] args) {
+    	   
+    	   Scanner s = new Scanner(System.in); 
+    	   double radius = s.nextDouble();
+    	   double perimeter;
+    	   double area;
+    	   	
+    	   if(radius <= 0) {
+    	   		System.out.println("Please enter a non-zero positive number: ");
+    	   		radius = s.nextDouble();
+    	   	}      
+    	   	
+    	   	perimeter = 2 * (Math.PI * radius);
+    	   	area = Math.PI * (radius * radius);
+    	   	
+    	   	System.out.println(perimeter);
+    	   	System.out.print(area);
+       
+       }
 }
--- W1PA/src/W1PA3.java
+++ W1PA/src/W1PA3.java
@@ -1,24 +1,25 @@
-// For a given Radius calculate the Perimeter and Area of a circle.
-import java.util.Scanner;  
+import java.util.Scanner;
+// Find the Sum of numbers which are divided by both 2 and 3 without a remainder up to a user given n number.
 
 public class W1PA3 {
-       public static void main(String[] args) {
-    	   
-    	   Scanner s = new Scanner(System.in); 
-    	   double radius = s.nextDouble();
-    	   double perimeter;
-    	   double area;
-    	   	
-    	   if(radius <= 0) {
-    	   		System.out.println("Please enter a non-zero positive number: ");
-    	   		radius = s.nextDouble();
-    	   	}      
-    	   	
-    	   	perimeter = 2 * (Math.PI * radius);
-    	   	area = Math.PI * (radius * radius);
-    	   	
-    	   	System.out.println(perimeter);
-    	   	System.out.print(area);
        
-       }
+	public static void main(String[] args) {
+	   Scanner sc = new Scanner(System.in);
+	   int n=sc.nextInt();
+	   int sum=0;	
+	   int counter = 1;
+		  for(int i =0; counter <= n; i++) {
+		      
+			     if((i % 2) == 0) {	
+			    
+			       if((i % 3) == 0) {	
+			    	 sum += i;
+	               }
+	                 counter += 1;
+			     }	               
+	               
+	        }        
+		   
+		    System.out.print(sum);   
+      }
 }
