56435a638a6ca189c88c63c24c0d93f7a21a46bf
D Rao <tdayakarrao@gmail.com>
2022-12-19 13:07:35 +0530

    Week 12: Programming Assignments

--- W12PA/src/W12PA4.java
+++ W12PA/src/W12PA4.java
@@ -1,4 +1,27 @@
+class Parent {
+    
+	public static void testClassMethod() {
+        System.out.println("The static method.");
+    }
+    public void testInstanceMethod() {
+        System.out.println("The instance method.");
+    }
+}
+public class W12PA4 extends Parent { //Child class
+   
+	public static void testClassMethod() { }
 
-public class W12PA4 {
-
+	public static void main(String[] args) {
+  
+		W12PA4 ch = new W12PA4(); // Creating child class type object
+        
+     // Call the instance method in the Parent class 
+  
+		ch.testInstanceMethod();
+		
+     // Call the static method in the Parent class
+  
+		Parent.testClassMethod();
+  
+  }
 }
