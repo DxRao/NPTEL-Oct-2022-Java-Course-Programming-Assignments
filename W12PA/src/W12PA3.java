faf449ab067d71cd92a45cc36820c674e6f34e08
D Rao <tdayakarrao@gmail.com>
2022-12-19 13:01:38 +0530

    Week 12: Programming Assignments

--- W12PA/src/W12PA3.java
+++ W12PA/src/W12PA3.java
@@ -1,4 +1,43 @@
+import java.util.*;
 
-public class W12PA3 {
+public class W12PA3{
+ 
+	public static void main(String[] args){
+      
+		Scanner s1 = new Scanner(System.in);
 
+   //Read your first name in string
+	String fname = "";
+
+    fname = s1.nextLine();
+    
+    //Read  your last name in string
+	
+	String lname =  "";
+
+	lname = s1.nextLine();
+   
+     //Read  rollnumber in integer
+
+	int  rnumber = s1.nextInt();
+    
+    //Read  1st subject mark in float
+
+	float sub1 = s1.nextFloat();
+    
+    //Read  2nd subject mark in float
+
+	float sub2 = s1.nextFloat();
+	
+    // find the avg mark in the subject
+
+	float avg;
+	avg = (sub1 + sub2) / 2;
+
+    //Print the record in the format: name+" "+rollnumber+" "+avg_mark 
+    
+	System.out.print(fname + lname + " " + rnumber + " " + avg);
+           
+
+    }
 }
